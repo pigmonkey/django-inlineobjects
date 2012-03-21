9a51a001090245aeea43b51391d9af87a38bece0
Pig Monkey <pm@pig-monkey.com>
2012-03-21 13:43:16 -0700

    A Javascript-based interface to add inlines is now included.
    The original javascript was written by Nathan Borror for his
    django-basic-apps (which django-inlines is forked from). The
    functionality to insert the inline at the current cursor position comes
    from [gclagorn's Stack Overflow
    answer](http://stackoverflow.com/a/1064139).

--- README.md
+++ README.md
@@ -37,6 +37,22 @@ To render this in a template, you would use the template filter.
 This would insert the `media.photo` object with an ID of `1` into the post body.
 
 
+### Admin
+
+A javascript-based interface to add inlines can be added to the Django admin site using the included template. The included template must be added to the relevant model's change form.
+
+For example, to add the interface to the `body` field of the `Post` model in an application called `blog`, you would create the template `admin/blog/post/change_form.html`. The content of that template would contain the following:
+
+    {% extends "admin/change_form.html" %}
+
+    {% block extrahead %}
+        {{ block.super }}
+        {% include 'inlines/inlines.js' with field='post' %}
+    {% endblock %}
+
+This template expands the default `admin/change_form.html` template, including `inlines/inlines.js` in the `extrahead` block. The included template expects the variable `field` to be included in the context. The variable should be set to the name of the model field that you want the inlines to be applied to.
+
+
 ### Templates
 
 How content is rendered is determined by a template. These templates should be stored within a template directory called `inlines` and use the filename structure `<app_name>_<model_name>.html`. The above usage example, for instance, would look for a template called `templates/media_photo.html`.
---
+++ inlines/templates/inlines/inlines.js
@@ -0,0 +1,81 @@
+{% load adminmedia inlines_tags %}
+<script type="text/javascript">
+    function InlineInit() {
+        var textarea = document.getElementById('id_{{ field }}');
+        var target_div = textarea.parentNode;
+        var content = ''
+        content += '{% get_inline_types as inline_list %}'
+        content += '<label>{{ field|capfirst }} inlines:</label>'
+
+        content += '<strong>Inline type:</strong> '
+        content += '<select id="id_inline_content_type" onchange="document.getElementById(\'lookup_id_inline\').href = \'../../../\'+this.value+\'/\';" style="margin-right:20px;">'
+        content += '  <option>----------</option>'
+        content += '  {% for inline in inline_list %}'
+        content += '  <option value="{{ inline.content_type.app_label }}/{{ inline.content_type.model }}">{{ inline.content_type.app_label|capfirst }}: {{ inline.content_type.model|capfirst }}</option>'
+        content += '  {% endfor %}'
+        content += '</select> '
+
+        content += '<strong>Object:</strong> '
+        content += '<input type="text" class="vIntegerField" id="id_inline" size="10" /> '
+        content += '<a id="lookup_id_inline" href="#" class="related-lookup" onclick="if(document.getElementById(\'id_inline_content_type\').value != \'----------\') { return showRelatedObjectLookupPopup(this); }" style="margin-right:20px;"><img src="{% admin_media_prefix %}img/admin/selector-search.gif" width="16" height="16" alt="Loopup" /></a> '
+
+        content += '<strong>Class:</strong> '
+        content += '<select id="id_inline_class">'
+        content += '  <option value="small_left">Small left</option>'
+        content += '  <option value="small_right">Small right</option>'
+        content += '  <option value="medium_left">Medium left</option>'
+        content += '  <option value="medium_right">Medium right</option>'
+        content += '  <option value="large_left">Large left</option>'
+        content += '  <option value="large_right">Large right</option>'
+        content += '  <option value="full">Full</option>'
+        content += '</select>'
+
+        content += '<input type="button" value="Add" style="margin-left:10px;" onclick="return insertInline(document.getElementById(\'id_inline_content_type\').value, document.getElementById(\'id_inline\').value, document.getElementById(\'id_inline_class\').value)" />'
+        content += '<p class="help">Insert inlines into your {{ field }} by choosing an inline type, then an object, then a class.</p>'
+
+        var div = document.createElement('div');
+        div.setAttribute('style', 'margin-top:10px;');
+        div.innerHTML = content;
+
+        target_div.insertBefore(div, textarea.nextSibling);
+    }
+
+    function insertInline(type, id, classname) {
+        if (type != '----------' && id != '') {
+            var inline = '<inline type="'+type.replace('/', '.')+'" id="'+id+'" class="'+classname+'" />';
+            var textarea = document.getElementById('id_{{ field }}');
+            var scrollPos = textarea.scrollTop;
+            var strPos = 0;
+            var br = ((textarea.selectionStart || textarea.selectionStart == '0') ? 
+                "ff" : (document.selection ? "ie" : false ) );
+        if (br == "ie") { 
+            textarea.focus();
+            var range = document.selection.createRange();
+            range.moveStart ('character', -textarea.value.length);
+            strPos = range.text.length;
+        }
+        else if (br == "ff") strPos = textarea.selectionStart;
+
+        var front = (textarea.value).substring(0,strPos);  
+        var back = (textarea.value).substring(strPos,textarea.value.length); 
+        textarea.value=front+inline+back;
+        strPos = strPos + inline.length;
+        if (br == "ie") { 
+            textarea.focus();
+            var range = document.selection.createRange();
+            range.moveStart ('character', -textarea.value.length);
+            range.moveStart ('character', strPos);
+            range.moveEnd ('character', 0);
+            range.select();
+        }
+        else if (br == "ff") {
+            textarea.selectionStart = strPos;
+            textarea.selectionEnd = strPos;
+            textarea.focus();
+        }
+        textarea.scrollTop = scrollPos;
+      }
+    }
+
+    addEvent(window, 'load', InlineInit);
+</script>
