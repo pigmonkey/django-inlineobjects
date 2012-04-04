be9cf839eef7b2b529c4690d1dc8c321f35c2039
Pig Monkey <pm@pig-monkey.com>
2012-04-04 11:53:03 -0700

    Javascript now uses separate size and alignment attritbutes

--- inlines/templates/inlines/inlines.js
+++ inlines/templates/inlines/inlines.js
@@ -19,18 +19,25 @@
         content += '<input type="text" class="vIntegerField" id="id_inline" size="10" /> '
         content += '<a id="lookup_id_inline" href="#" class="related-lookup" onclick="if(document.getElementById(\'id_inline_content_type\').value != \'----------\') { return showRelatedObjectLookupPopup(this); }" style="margin-right:20px;"><img src="{% admin_media_prefix %}img/admin/selector-search.gif" width="16" height="16" alt="Lookup" /></a> '
 
-        content += '<strong>Class:</strong> '
-        content += '<select id="id_inline_class">'
-        content += '  <option value="small_left">Small left</option>'
-        content += '  <option value="small_right">Small right</option>'
-        content += '  <option value="medium_left">Medium left</option>'
-        content += '  <option value="medium_right">Medium right</option>'
-        content += '  <option value="large_left">Large left</option>'
-        content += '  <option value="large_right">Large right</option>'
-        content += '  <option value="full">Full</option>'
+        content += '<strong>Size:</strong> '
+        content += '<select id="id_inline_size" style="margin-right:20px;">'
+        content += '  <option>----------</option>'
+        content += '  <option value="thumbnail">Thumbnail</option>'
+        content += '  <option value="small">Small</option>'
+        content += '  <option value="medium">Medium</option>'
+        content += '  <option value="large">Large</option>'
+        content += '  <option value="original">Original</option>'
         content += '</select>'
 
-        content += '<input type="button" value="Add" style="margin-left:10px;" onclick="return insertInline(document.getElementById(\'id_inline_content_type\').value, document.getElementById(\'id_inline\').value, document.getElementById(\'id_inline_class\').value)" />'
+        content += '<strong>Align:</strong> '
+        content += '<select id="id_inline_align">'
+        content += '  <option>----------</option>'
+        content += '  <option value="left">Left</option>'
+        content += '  <option value="right">Right</option>'
+        content += '  <option value="center">Center</option>'
+        content += '</select>'
+
+        content += '<input type="button" value="Add" style="margin-left:20px;" onclick="return insertInline(document.getElementById(\'id_inline_content_type\').value, document.getElementById(\'id_inline\').value, document.getElementById(\'id_inline_size\').value, document.getElementById(\'id_inline_align\').value)" />'
         content += '<p class="help">Insert inlines into your {{ field }} by choosing an inline type, then an object, then a class.</p>'
 
         var div = document.createElement('div');
@@ -40,40 +47,52 @@
         target_div.insertBefore(div, textarea.nextSibling);
     }
 
-    function insertInline(type, id, classname) {
+    function insertInline(type, id, size, align) {
         if (type != '----------' && id != '') {
-            var inline = '<inline type="'+type.replace('/', '.')+'" id="'+id+'" class="'+classname+'" />';
+            if (size != '----------' ) {
+                var size_attribute = ' size="'+size+'"';
+            } else {
+                var size_attribute = '';
+            }
+
+            if (align != '----------' ) {
+                var align_attribute = ' align="'+align+'"';
+            } else {
+                var align_attribute = '';
+            }
+
+            var inline = '<inline type="'+type.replace('/', '.')+'" id="'+id+'"'+size_attribute+''+align_attribute+' />';
             var textarea = document.getElementById('id_{{ field }}');
             var scrollPos = textarea.scrollTop;
             var strPos = 0;
             var br = ((textarea.selectionStart || textarea.selectionStart == '0') ? 
                 "ff" : (document.selection ? "ie" : false ) );
-        if (br == "ie") { 
-            textarea.focus();
-            var range = document.selection.createRange();
-            range.moveStart ('character', -textarea.value.length);
-            strPos = range.text.length;
-        }
-        else if (br == "ff") strPos = textarea.selectionStart;
+            if (br == "ie") { 
+                textarea.focus();
+                var range = document.selection.createRange();
+                range.moveStart ('character', -textarea.value.length);
+                strPos = range.text.length;
+            }
+            else if (br == "ff") strPos = textarea.selectionStart;
 
-        var front = (textarea.value).substring(0,strPos);  
-        var back = (textarea.value).substring(strPos,textarea.value.length); 
-        textarea.value=front+inline+back;
-        strPos = strPos + inline.length;
-        if (br == "ie") { 
-            textarea.focus();
-            var range = document.selection.createRange();
-            range.moveStart ('character', -textarea.value.length);
-            range.moveStart ('character', strPos);
-            range.moveEnd ('character', 0);
-            range.select();
-        }
-        else if (br == "ff") {
-            textarea.selectionStart = strPos;
-            textarea.selectionEnd = strPos;
-            textarea.focus();
-        }
-        textarea.scrollTop = scrollPos;
+            var front = (textarea.value).substring(0,strPos);  
+            var back = (textarea.value).substring(strPos,textarea.value.length); 
+            textarea.value=front+inline+back;
+            strPos = strPos + inline.length;
+            if (br == "ie") { 
+                textarea.focus();
+                var range = document.selection.createRange();
+                range.moveStart ('character', -textarea.value.length);
+                range.moveStart ('character', strPos);
+                range.moveEnd ('character', 0);
+                range.select();
+            }
+            else if (br == "ff") {
+                textarea.selectionStart = strPos;
+                textarea.selectionEnd = strPos;
+                textarea.focus();
+            }
+            textarea.scrollTop = scrollPos;
       }
     }
 
