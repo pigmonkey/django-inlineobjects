622e53ed8acfbedbce29c889ec54f42344fe7bd8
Pig Monkey <pm@pig-monkey.com>
2012-08-02 15:11:37 -0700

    PEP8

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'django-inlineobjects',
-    packages = find_packages(),
-    version = '1.0.1',
-    description = 'A reusable Django application used to insert content objects into other pieces of content.',
-    author = 'Peter Hogg',
-    author_email = 'peter@havenaut.net',
-    url = 'https://github.com/pigmonkey/django-inlineobjects',
-    classifiers = [
+    name='django-inlineobjects',
+    packages=find_packages(),
+    version='1.0.1',
+    description='A reusable Django application used to insert content objects into other pieces of content.',
+    author='Peter Hogg',
+    author_email='peter@havenaut.net',
+    url='https://github.com/pigmonkey/django-inlineobjects',
+    classifiers=[
         "Programming Language :: Python",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Development Status :: 5 - Production/Stable",
         "Framework :: Django",
     ],
-    long_description = open('README.md').read(),
-    include_package_data = True,
+    long_description=open('README.md').read(),
+    include_package_data=True,
     zip_safe=False,
-    install_requires = ['BeautifulSoup>=3.0.0'],
+    install_requires=['BeautifulSoup>=3.0.0'],
 )
