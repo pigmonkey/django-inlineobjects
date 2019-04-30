c8b4c94b6bacb4217550f7ae9b3ce12c8290b189
Pig Monkey <pm@pig-monkey.com>
2019-04-30 13:41:58 -0700

    explicitly set on_delete

--- inlines/models.py
+++ inlines/models.py
@@ -6,7 +6,10 @@ from django.contrib.contenttypes.models import ContentType
 class InlineType(models.Model):
     """InlineType model"""
     title = models.CharField(max_length=200)
-    content_type = models.ForeignKey(ContentType)
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+    )
 
     class Meta(object):
         db_table = 'inline_types'
