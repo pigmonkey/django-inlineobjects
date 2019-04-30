23d0d0d6ad96f2215b374c29fd5cf70ab1a9e7f6
Pig Monkey <pm@pig-monkey.com>
2019-04-30 13:44:38 -0700

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='django-inlineobjects',
     packages=find_packages(),
-    version='2.0.0',
+    version='2.0.1',
     description='A reusable Django application used to insert content objects into other pieces of content.',
     author='Peter Hogg',
     author_email='peter@havenaut.net',
